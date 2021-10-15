ce5fc9c1b0a534b756ac7391ac1e012ed989d452
Fowellsoft <32065602+fowellsoft@users.noreply.github.com>
2021-10-15 13:55:30 +0200

    Con validaciones

--- amigos/js/amigos_controller.js
+++ amigos/js/amigos_controller.js
@@ -8,17 +8,17 @@ app.controller('amigosCtrl', function($scope, $window)
     $scope.j = 0;
     $scope.insertValue = ((value) =>
     {
-        let textAux = $scope.matrizAmigo[$scope.i][$scope.j].valor;
+        let textAux = $scope.matrizAmigo[$scope.i][$scope.j].sumando;
         if(value == "B")
         {
             if(textAux && textAux.length > 0)
             {
                 textAux = textAux.substring(0, textAux.length-1);
-                $scope.matrizAmigo[$scope.i][$scope.j].valor = textAux;
+                $scope.matrizAmigo[$scope.i][$scope.j].sumando = textAux;
             }
         }
         else
-            $scope.matrizAmigo[$scope.i][$scope.j].valor += value;
+            $scope.matrizAmigo[$scope.i][$scope.j].sumando += value;
     });
 
     $scope.selectField = ((i, j) =>
@@ -38,6 +38,59 @@ app.controller('amigosCtrl', function($scope, $window)
         return ($scope.i == i && $scope.j == j);
     });
 
+    $scope.validaResultados = (() => {
+
+        $scope.resultadoOperaciones = { someoneWithError: false,
+                                        finalMessage: ""
+                                      };
+
+        // Para cada una de las filas realiza la suma de los sumandos
+        for(let i=0; i < $scope.matrizAmigo.length; i++)
+        {
+            let sumandosList = $scope.matrizAmigo[i];
+            let showError = true;
+
+            // Todos sumandos tienen valor
+            let aux = sumandosList.filter(e => (e && e.sumando && e.sumando != ""));
+            if(aux.length >= 2)
+            {
+                let total = 0;
+                for(let j=0; j < sumandosList.length; j++)
+                {
+                    total += Number(sumandosList[j].sumando);
+                }
+
+                if(total == $scope.numAmigo)
+                {   showError = false; }
+                else
+                {   
+                    $scope.resultadoOperaciones.someoneWithError = true;
+                    $scope.resultadoOperaciones.finalMessage = "Revisa las operaciones..."
+                }
+            }
+            else
+            {
+                $scope.resultadoOperaciones.someoneWithError = true;
+                if($scope.resultadoOperaciones.finalMessage == "")
+                    $scope.resultadoOperaciones.finalMessage = "Completa las operaciones..."
+            }
+
+            
+            if(showError)
+            {
+                // Filtra el editable
+                let sumandoEditable = sumandosList.filter(e => (e && e.editable));
+                sumandoEditable.withError = true;
+            }
+        }
+
+        if(!$scope.resultadoOperaciones.someoneWithError)
+        {
+            $scope.resultadoOperaciones.finalMessage = "Bien hecho! Sigue practicando.";
+        }
+
+    });
+
     // ----------------------------------------------------------------------------
 
     let randomIntFromInterval = ((min, max) =>
@@ -45,9 +98,12 @@ app.controller('amigosCtrl', function($scope, $window)
         return Math.floor(Math.random() * (max - min + 1) + min)
     });
 
-
     // Podrán ser la valores finales como (10, 100, 1000) o números aleatorios dentro 
-    $scope.numAmigo = 10;
+    $scope.numAmigo = 1000;
+    let numLastre = 10;
+    if((numLastre != 1 && $scope.numAmigo%10 != 0 && numLastre%10 != 0) ||
+       (numLastre >= $scope.numAmigo))
+        numLastre = 1;
 
     // Número amigo <=10 => siempre derecha, numAmigo+1 (por el 0)
     // Número amigo >10 => alterna derecha e izquierda, numFilas configurable por defecto 10
@@ -58,8 +114,8 @@ app.controller('amigosCtrl', function($scope, $window)
     {
         for(let i=0; i <= $scope.numAmigo; i++)
         {
-            $scope.matrizAmigo[i] = [{ "valor": "", "editable": true},
-                                     { "valor": "", "editable": true}];
+            $scope.matrizAmigo[i] = [{ "sumando": "", "editable": true},
+                                     { "sumando": "", "editable": true}];
         }
     }
     else
@@ -67,26 +123,26 @@ app.controller('amigosCtrl', function($scope, $window)
         $scope.j = 1;
         for(let i=0; i < numRows; i++)
         {
-            let v = randomIntFromInterval(1, $scope.numAmigo);
+            // Evita que se repitan
+            let v = -1;
+            do
+            {
+                v = randomIntFromInterval(1, $scope.numAmigo/numLastre);
+            }
+            while($scope.matrizAmigo.find(m => m.find(e => e && e.sumando && e.sumando != "" && e.sumando == v*numLastre)))
+
             $scope.matrizAmigo[i] = [];
 
             if(((i+1)%2)!=0) // Si es impar
             {
-                $scope.matrizAmigo[i][0] = { "valor": v, "editable": false} ;
-                $scope.matrizAmigo[i][1] = { "valor": "", "editable": true};
+                $scope.matrizAmigo[i][0] = { "sumando": v*numLastre, "editable": false} ;
+                $scope.matrizAmigo[i][1] = { "sumando": "", "editable": true};
             }
             else // Si es par
             {
-                $scope.matrizAmigo[i][0] = { "valor": "", "editable": true};
-                $scope.matrizAmigo[i][1] = { "valor": v, "editable": false};
+                $scope.matrizAmigo[i][0] = { "sumando": "", "editable": true};
+                $scope.matrizAmigo[i][1] = { "sumando": v*numLastre, "editable": false};
             }
         }
     }
-
-    $scope.isEditable = ((i, j) => {
-
-
-
-    });
-
 });
